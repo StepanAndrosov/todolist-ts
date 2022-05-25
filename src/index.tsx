e03c3fb4e757c18b97e9fd91fb09cffa29bffb81
Androsov-Stepan <stapan.news@yandex.ru>
2022-05-25 09:35:29 +0300

    router changed

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,14 @@ import * as serviceWorker from './serviceWorker';
 import {App} from "./app/App";
 import {Provider} from "react-redux";
 import {store} from "./app/store";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const rerenderEntireTree = () => {
     ReactDOM.render(
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <App/>
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
         , document.getElementById('root'));
 }
